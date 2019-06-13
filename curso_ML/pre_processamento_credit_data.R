621c3152da2db11b38fe79191a186e03ae2ce61b
henrikots <henrikots@gmail.com>
2019-06-12 22:35:01 -0300

    Normalização

--- curso_ML/pre_processamento_credit_data.R
+++ curso_ML/pre_processamento_credit_data.R
@@ -12,4 +12,12 @@ mean(base$age[base$age > 0], na.rm = TRUE)
 base$age <- ifelse(base$age < 0, 40.92, base$age)
 
 #ajusta valores faltantes com a média
-base$age <- ifelse(is.na(base$age), mean(base$age) ,base$age)
+base$age <- ifelse(is.na(base$age), mean(base$age, na.rm = TRUE) ,base$age)
+
+#normalização
+# x = (x - min(x)) / (max(x) - min(x))
+
+#padronização - mais recomendado por tratar os outliers
+# x = (x - media(x)) / (desvio_padrao(x)) 
+
+base[, 1:3] <- scale(base[, 1:3]) 

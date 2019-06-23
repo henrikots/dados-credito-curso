1706e12247cd51c47cc59b3bcf939f8e49066569
henrikots <henrikots@gmail.com>
2019-06-23 14:36:39 -0300

    Implementacao  SVM

--- curso_ML/svm_credit_data.R
+++ curso_ML/svm_credit_data.R
@@ -31,3 +31,26 @@ divisao <- sample.split(base$default, SplitRatio = 0.75)
 
 base_treinamento <- subset(base, divisao == TRUE)
 base_teste <- subset(base, divisao == FALSE)
+
+
+library(e1071)
+classificador = svm(formula = default ~ ., data = base_treinamento, type = "C-classification", 
+                    kernel = "radial", cost = 8)
+
+previsoes = predict(classificador, newdata = base_teste[-4])
+
+
+matriz_confusao = table(base_teste[,4], previsoes)
+
+library(caret)
+confusionMatrix(matriz_confusao)
+
+#linear = 94.8%
+#polinomyal = 96.5%
+#radial = 97.8%
+#sigmoid = 84.6% 
+
+#ajuste de custo para o radial
+#0.2 = 97.2%
+#5 = 98.8%
+#8 = 99%
